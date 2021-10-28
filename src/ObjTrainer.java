8223670977167a19344d927d12fc15047afed754
Lukas Radtke <lukas.radtke2000@gmail.com>
2021-10-28 23:51:33 +0200

    Aufgaben für die Kapitel Tests

--- src/ObjTrainer.java
+++ src/ObjTrainer.java
@@ -1,27 +1,9 @@
 public class ObjTrainer {
     public static void main(String[] args) {
-            Flasche f1 = new Flasche();
-            f1.setInhalt("Cola");
-            Flasche f2 = new Flasche();
-            Flasche f3 = new Flasche();
-            f1.setVolumen(1.0);
-            f2.setInhalt("Fanta");
-            f3.setInhalt(f1.getInhalt());
-            f2.setVolumen(0.5);
-            f3.setVolumen(f1.getVolumen()+f2.getVolumen());
-            f2.setInhalt(f1.getInhalt());
-
-        System.out.println(f1.getVolumen());
-        System.out.println(f1.getInhalt());
-
-        System.out.println("------------------");
-
-        System.out.println(f2.getVolumen());
-        System.out.println(f2.getInhalt());
-
-        System.out.println("------------------");
-
-        System.out.println(f3.getVolumen());
-        System.out.println(f3.getInhalt());
+        KlasseA a1=new KlasseA();
+        System.out.println(a1.calc(2));
+        System.out.println(a1.calc(2,4));
+        System.out.println(a1.calc(a1.calc(2, 3),4));
+        System.out.println(a1.calc(a1.calc(2, 1),a1.calc()));
         }
 }
