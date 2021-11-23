9992a81c8c678fe73644086fa0d5af1cac192ae6
Lukas Radtke <lukas.radtke2000@gmail.com>
2021-11-23 10:27:09 +0100

    yoyoy

---
+++ src/MouseBot.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class MouseBot {
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        Robot robot = new Robot();
+        while (true) {
+            robot.mouseMove(400, 400);
+            Thread.sleep(5000);
+        }
+    }
+}
